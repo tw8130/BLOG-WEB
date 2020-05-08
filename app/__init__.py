9a4de63b55924d2494914c67f88e9a1fc25f724d
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-08 17:53:57 +0300

    create a create_app() function that takes the configuration setting key as an argument then create the Flask app instance then call the init_app() on an extension to complete on their initialization.Finally we return app

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,13 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 
 bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
