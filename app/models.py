e265030841dd0cc2b7f20b330e71a5c2e13f9adb
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-08 22:17:54 +0300

    we create relationship btween role and users class

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'User {self.username}'
@@ -20,6 +21,7 @@ class Role(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
 
     def __repr__(self):
         return f'User {self.name}'
