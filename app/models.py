a464d4f303df564b03c95f5eef33426c48443f30
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-08 22:05:32 +0300

    create  Role class that will define all the different roles. We create two columns for the ID and the name

--- app/models.py
+++ app/models.py
@@ -2,9 +2,24 @@ from . import db
 
 
 class User(db.Model):
+    '''
+    Class to create new users
+    '''
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Role(db.Model):
+    '''
+    Class to create roles for our new users
+    '''
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
