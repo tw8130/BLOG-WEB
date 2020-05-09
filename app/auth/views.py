777260a5933007f802202ff913f69cf415adcb63
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-09 13:41:13 +0300

    we create an authenticated route logout that calls the flask_login's logout_userfunction. This will logout the user from our application. We then redirect the user to the index page

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,redirect,url_for,flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required,
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
@@ -17,6 +17,12 @@ def login():
 
     title = "watchlist login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
 
 
 @auth.route('/register',methods = ["GET","POST"])
