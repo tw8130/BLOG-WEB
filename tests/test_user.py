f459286f3a0fd704365b10844f45ece19462db8e
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-09 12:19:22 +0300

    use setup method to create instance of user class then use test_password_setter to ascertain if password is being hashed

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,3 +5,15 @@ class UserModelTest(unittest.TestCase):
     '''
     Test to test behaviour of user model class
     '''
+
+    def setUp(self):
+        '''
+        Method that creates instance of user class and pass paswword property
+        '''
+        self.new_user = User(password = 'banana')
+
+    def test_password_setter(self):
+        '''
+        Acsertains if password is being hashed  and if pass_secure contains a value
+        '''
+        self.assertTrue(self.new_user.password_hash is not None)
