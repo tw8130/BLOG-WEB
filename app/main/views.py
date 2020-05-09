a44d451cf5523e9a135065edf6af4e4ccf0966e9
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-09 15:10:05 +0300

    import the abort function, that stops a request, and returns a response according to the status code passed in then query the database to find the user according to the username passed then f no user is found the abort is called and a 404 status code is returned as a response

---
+++ app/main/views.py
@@ -0,0 +1,14 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from flask_login import login_required,current_user
+from ..models import User
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
