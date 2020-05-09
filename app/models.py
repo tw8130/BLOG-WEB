2254b9660107ec8c74025ac9d0847ef5607a70ef
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-09 12:11:59 +0300

    use property method to with will raise an attribute error then password setter maethod to ensure that password is passed as avalue to password_hash column then use verify password function to verify if password is correct

--- app/models.py
+++ app/models.py
@@ -12,6 +12,17 @@ class User(db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(255))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
 
     def __repr__(self):
