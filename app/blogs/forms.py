4a9903f39f375addd276f43861f02675e7646cc7
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-10 21:39:12 +0300

    create postform and commentform each with their input fields

--- app/blogs/forms.py
+++ app/blogs/forms.py
@@ -1,3 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,TextAreaField,ValidationError
 from wtforms.validators import Required
+
+class PostForm(FlaskForm):
+    title = StringField('Title',validators=[Required()])
+    body = TextAreaField('Text',validators=[Required()])
+    submit = SubmitField("Submit")
+
+
+class CommentForm(FlaskForm):
+    body = TextAreaField('Text',validators=[Required()])
+    submit = SubmitField("Submit")
