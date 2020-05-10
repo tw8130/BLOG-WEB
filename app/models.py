a7a42af5036fa22475942a47d792103cadb3dd64
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-05-10 20:43:28 +0300

    create blog and comment models and create relationship with users table

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 from . import login_manager
 
 @login_manager.user_loader
@@ -20,6 +21,11 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
+
+    # Relationship with the Blog Post table 
+    posts = db.relationship('Blog', backref = 'author', lazy = "dynamic")
+    # Relationship with the Comments table
+    comments = db.relationship('Comment', backref='author', lazy='dynamic')
 
     @property
     def password(self):
@@ -56,3 +62,36 @@ class Quote:
         self.author = author 
         self.quote  = quote
         self.permalink =  permalink
+
+class Blog(db.Model):
+    __tablename__ = "blogs"
+    '''
+    Class for blog posts
+    '''
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'), nullable = False)
+    posted = db.Column(db.DateTime, index=True, nullable = False, default=datetime.utcnow)
+    title = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.Text, nullable = False)
+
+    # Relationship with the user table
+    users = db.relationship(User)
+    
+    # Relationship with the Comments table 
+    comments = db.relationship('Comment', backref='blogs', lazy='dynamic')
+    
+    def __repr__(self):
+        return f"POST ID:{self.id} -- Date: {self.posted}"  
+
+class Comment(db.Model):
+    '''
+    Class for the comment section
+    '''
+
+    __tablename__='comments'
+
+    id  = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String)
+    posted = db.Column(db.DateTime, default = datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    blog_id = db.Column(db.Integer, db.ForeignKey("blogs.id"))
